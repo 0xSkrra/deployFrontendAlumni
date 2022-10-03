29909180def2954abb589594d05abe6beb603230
Espen Sjo <espen.sjo@no.experis.com>
2022-10-03 15:07:59 +0200

    refactor: typescript & add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
