34bcc83f46e5ecb6a7dcd24e8f5ebce7c3598889
Espen Sjo <espen.sjo@no.experis.com>
2022-10-03 15:15:07 +0200

    refactor: remove unneccessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1 className="text-3xl font-bold underline text-red-600">
           Edit <code>src/App.tsx</code> and save to reload.
         </h1>
