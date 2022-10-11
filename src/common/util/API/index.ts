13d08ebdacb2d50d5b57a3a442ff65b12e812b98
Espen Sjo <esjo20@gmail.com>
2022-10-11 12:15:32 +0200

    feat: create patch and get by id methods for alumni api users

--- src/common/util/API/index.ts
+++ src/common/util/API/index.ts
@@ -5,3 +5,13 @@ export const getUserProfile = async (): Promise<UserProfile> => {
     const user: UserProfile =  (await axios.get('/api/users/1')).data
     return user
 }
+
+export const updateUserProfile = async (user: UserProfile): Promise<UserProfile> => {
+    const patchedUser: UserProfile = (await axios.patch(`/api/users/${user.id}`, user)).data
+    return patchedUser
+}
+
+export const getUserById = async (id: number): Promise<UserProfile> => {
+    const user: UserProfile = (await axios.get(`/api/users/${id}`)).data
+    return user
+}
