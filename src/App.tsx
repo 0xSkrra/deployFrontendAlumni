be7cdd43ffdb90038ca200bfa903a3c595c05bfd
Espen Sjo <esjo20@gmail.com>
2022-10-12 14:15:33 +0200

    add private routes and configure private pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,45 @@ import AccountPage from './components/AccountPage';
 import AccountSettings from './components/AccountSettingsPage';
 import Layout from './components/Layout';
 import StartPage from './components/StartPage';
+import PrivateRoute from './routes/utils';
 import Dashboard from './view/Dashboard';
 
 function App() {
   const { initialized } = useKeycloak()
+  
 
   if (!initialized) {
     return <div>Loading...</div>
   }
+
   
   return (
     <BrowserRouter> 
     <Layout>
       <Routes>
         <Route path="/" element={<StartPage />} /> 
-        <Route path="/account/settings" element={<AccountSettings />}/>
-        <Route path='/timeline' element={<Dashboard />}/>
-        <Route path="/account/:id" element={<AccountPage />}/>  
-        <Route path="/account" element={<AccountPage />}/>       
+
+          <Route path="/account/settings" element={
+            <PrivateRoute>
+              <AccountSettings />
+            </PrivateRoute>
+          }/>
+          <Route path='/timeline' element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }/>
+          <Route path="/account/:id" element={
+            <PrivateRoute>
+              <AccountPage />
+            </PrivateRoute>
+            }/>  
+          <Route path="/account" element={
+            <PrivateRoute>
+              <AccountPage />
+            </PrivateRoute>
+            }/>       
+
       </Routes>
       </Layout>  
     </BrowserRouter>
