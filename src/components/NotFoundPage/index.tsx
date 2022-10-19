9b6922ca1d8bfe5c2fed92e1839040ea76e0ce08
Fredrik Fauskanger <ffauskanger@gmail.com>
2022-10-19 11:28:41 +0200

    feat: 404 page

---
+++ src/components/NotFoundPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+
+  return (
+    <>
+        <div className="flex h-screen justify-center text-7xl items-center"> 
+            <img src="https://stories.freepiklabs.com/storage/42123/oops-404-error-with-a-broken-robot-cuate-6558.png" alt="404 NOT FOUND"></img>
+        </div>
+    </>
+  )
+}
+
+export default NotFoundPage
