1e000a163486d9cfef2702f5b54e06a84f45de64
Espen Sjo <esjo20@gmail.com>
2022-10-20 09:42:04 +0200

    refactor: modal to display posts now looks much better

--- src/components/util/postItem.tsx
+++ src/components/util/postItem.tsx
@@ -1,42 +1,100 @@
 import { format } from 'path'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Group } from '../../common/interface/Group'
 import { Post } from '../../common/interface/Post'
+import { Topic } from '../../common/interface/Topic'
+import { Event } from '../../common/interface/Event'
+import { defaultUserProfile, UserProfile } from '../../common/interface/UserProfile'
+import { getUserById } from '../../common/util/API'
+import { useBoundStore } from '../../common/util/Store/Store'
+import RefactorPostModal from './refactorPostModal'
 interface postItemProps{
     post: Post
     onClickPost: () => void
 }
-const postItem = ({post, onClickPost}: postItemProps) => {
+const PostItem = ({post, onClickPost}: postItemProps) => {
+    const store = useBoundStore((state)=> state) 
+    const navigate = useNavigate()
+    const [targetString, setTargetString] = useState<string>('')
+    const [targetRedirect, setTargetRedirect] = useState<string>('')
+    const [showModal, setShowModal] = useState<boolean>(false)
 
+    const date = post.lastUpdated.replace('T',' ').split('.')[0]
+    useEffect(() => {
+        const getTarget = async () => {
+        if(post.groupId !== null){
+            const group = store.Groups.find((g) => g.id === post.groupId)
+            if(group !== undefined){
+                setTargetString(`Group/${group.title}`)
+                setTargetRedirect(`/groups/${group.id}`)
+            }
+        }else if( post.topicId !== null){
+            const topic = store.Topics.find((t) => t.id === post.topicId)
+            if(topic !== undefined){
+                setTargetString(`Topic/${topic.title}`)
+                setTargetRedirect(`/topics/${topic.id}`)
+            }
+        }else if( post.eventId !== null){
+            const event = store.Events.find((e) => e.id === post.eventId)
+            if(event !== undefined){
+                setTargetString(`Event/${event.name}`)
+                setTargetRedirect('/event')
+            }
+        }else if( post.receiverId !== null && post.receiverId !== undefined){
+            const user = (await getUserById(post.receiverId))
+            if(user !== undefined){
+                setTargetString(`User/${user.username}`)
+                setTargetRedirect(`/account/${user.id}`)
+            }
+        }
+    }
+    getTarget()
+    }, [post.eventId, post.groupId, post.receiverId, post.topicId, store.Events, store.Groups, store.Topics])
+    const handleOnClickTarget = () => {
+        setShowModal(!showModal)
+        navigate(targetRedirect)
+    }
+    let pic=post.author?.picture
+    if(pic===null) pic = "\\assets\\default_profile_img.jpg"
 
-const date = post.lastUpdated.replace('T',' ').split('.')[0]
-
-let pic=post.author?.picture
-if(pic===null) pic = "\\assets\\default_profile_img.jpg"
-
-  return (
-    <ol key={post.id} className="mt-2 divide-y divider-gray-200 max-h-full max-w-full">
-                <li onClick={onClickPost} >
-                    <div  className=" p-3 hover:cursor-pointer border border-gray-200 max-h-30 rounded-lg sm:flex hover:bg-gray-100 shadow-lg bg-slate-50">
-                        <div className="flex-col rounded">                
-                        <img className=" w-12 h-12 rounded-full mr-3 sm:mb-0" src={pic} alt=""/>
-                        </div>
-                        
-                        <div className="flex flex-col text-gray-600">
-                            <div className="text-base font-normal"><span className="font-medium text-gray-900 ">{post.title}</span></div>
-                            <div className="flex flex-row">
-                            <span className="inline-flex hover:text-blue-300 hover:cursor-pointer items-center text-xs font-normal text-gray-500">
-                                <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd"></path></svg>
-                            </span> 
-                            <span className="inline-flex ml-3 hover:text-blue-300 hover:cursor-pointer text-xs font-normal text-gray-500">
-                                Posted by: {post.author?.username} on {date}
-                            </span>
+    return (
+        <React.Fragment key={post.id}>
+            <ol key={post.id} className="mt-2 divide-y divider-gray-200 max-h-full max-w-full">
+                        <li onClick={() => setShowModal(!showModal)} >
+                            <div  className=" p-3 hover:cursor-pointer border border-gray-200 max-h-30 rounded-lg sm:flex hover:bg-gray-100 shadow-lg bg-slate-50">
+                                <div className="flex-col rounded">                
+                                <img className=" w-12 h-12 rounded-full mr-3 sm:mb-0" src={pic} alt=""/>
+                                </div>
+                                
+                                <div className="flex flex-col text-gray-600">
+                                    <div className="text-base font-normal"><span className="font-medium text-gray-900 ">{post.title}</span></div>
+                                    <div className="flex flex-row">
+                                    <span 
+                                    onClick={() => {handleOnClickTarget()}}
+                                    className="inline-flex hover:text-blue-300 hover:cursor-pointer items-center text-xs font-normal text-gray-900">
+                                        <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd"></path></svg>
+                                        {targetString}
+                                    </span> 
+                                    <span className="inline-flex ml-3 text-xs font-normal text-gray-500">
+                                        Posted by: <span onClick={() => navigate(`/account/${ post.author !== undefined ? post.author.id : -1}`)} className="inline-flex ml-1 mr-1 hover:text-blue-300 hover:cursor-pointer text-xs font-normal text-gray-900" >{post.author?.username}</span> on {date}
+                                    </span>
+                                    </div>
+                                    <button className="text-xs font-normal text-start hover:text-blue-300">Replies: {post.replies?.length}</button>
+                                </div>
                             </div>
-                            <button className="text-xs font-normal text-start hover:text-blue-300">Replies: {post.replies?.length}</button>
-                        </div>
-                    </div>
-                </li>
-            </ol>
+                        </li>
+                    </ol>
+
+                    <RefactorPostModal 
+                    showModal={showModal} 
+                    setShowModal={() => setShowModal(!showModal)}
+                    post={post}
+                    targetString={targetString}
+                    targetOnClick={handleOnClickTarget}
+                    />
+            </React.Fragment>
   )
 }
 
-export default postItem
+export default PostItem
---
+++ src/components/util/refactorPostModal.tsx
@@ -0,0 +1,107 @@
+import { CalendarEvent } from "kalend/common/interface";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Event } from "../../common/interface/Event";
+import { Group } from "../../common/interface/Group";
+import { Post } from "../../common/interface/Post";
+import { Topic } from "../../common/interface/Topic";
+import { UserProfile } from "../../common/interface/UserProfile";
+import { addCommentToPost } from "../../common/util/API";
+import Comments from "./comment";
+interface props{
+  showModal: boolean
+  setShowModal: () => void
+  post: Post
+  targetString: string
+  targetOnClick: () => void
+  
+}
+export default function CreateEventModal({
+    showModal, setShowModal, post, targetOnClick, targetString
+}   : props) {
+    const navigate = useNavigate()
+    const [newPostComment, setNewPostComment] = useState("")
+    const [comments, setComments] = useState(post.replies!)
+    const onSubmitkNewComment = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault()
+        if(newPostComment.length < 1) return
+        const newComment = await addCommentToPost(post.id ,newPostComment)
+        setComments((state) => [...state, newComment])
+        setNewPostComment("")
+      }
+    return (
+      <>
+        {showModal ? (
+          <>
+            <div
+              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            >
+              <div className="modal-overlay absolute w-[70%] h-full bg-white">
+                {/*content*/}
+                <div className="border-0 rounded-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-solid border-slate-200 rounded-t">
+                  <div className="flex flex-col">
+                  <span className="inline-flex text-xs font-normal text-gray-600">
+                  <span onClick={targetOnClick} className="inline-flex mr-1 hover:text-blue-300 hover:cursor-pointer text-xs font-normal text-gray-900" >
+                                            {targetString} </span>* Posted by: <span onClick={() => navigate(`/account/${ post.author !== undefined ? post.author.id : -1}`)} className="inline-flex ml-1 mr-1 hover:text-blue-300 hover:cursor-pointer text-xs font-normal text-gray-900" >
+                                            {post.author?.username}</span> on {post.lastUpdated.replace('T',' ').split('.')[0]}
+                    </span>
+                    <h3 className="text-3xl mt-1 inline-flex font-semibold">
+                      {post.title}
+                    </h3>
+                    </div>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={setShowModal}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        ×
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative min-h-full p-6 flex-auto">
+                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                      {post.body}
+                    </p>
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-center overflow-x-hidden p-6 border-t border-solid border-slate-200 rounded-b">
+                    <div className="w-[75%] px-3 mb-2 mt-2">
+                    <div className="mb-4 w-full flex-col bg-white">
+                            <Comments comments={comments}/>
+                        </div>
+                        <form onSubmit={(e) => onSubmitkNewComment(e)}>
+                            <div className="mb-4 w-full bg-white rounded-lg border-2 border-gray-300 ">
+                                <div className="py-2 px-4 bg-white rounded-t-lg ">
+                                    <label htmlFor="comment" className="sr-only">Your comment</label>
+                                    <textarea onChange={(e) => {
+                                        e.preventDefault()
+                                        setNewPostComment(e.target.value)
+                                    }} id="comment" value={newPostComment} rows={4} className="p-2  outline-0 border-b border-gray-200  w-full text-sm text-gray-900 bg-white" placeholder="Write a comment..." required></textarea>
+                                </div>
+                                <div className="flex justify-between items-center py-2 px-3 ">
+                                <button type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+                                        Post comment
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        
+                    </div>
+                    
+                  </div>
+                </div>
+                
+              </div>
+              
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+      </>
+    );
+  }
+
+  
