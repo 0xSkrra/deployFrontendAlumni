0bc45c5c784c755c758a296dcda389df4591d9f9
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-21 10:15:42 +0200

    feat: add create topic modal and group modal

--- src/components/CreateModal/CreateTopicModal.tsx
+++ src/components/CreateModal/CreateTopicModal.tsx
@@ -19,7 +19,7 @@ export default function CreateTopicModal() {
     return (
       <>
         <button
-          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          className="px-4 flex py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
           type="button"
           onClick={() => setShowModal(true)}
         >
--- src/components/GroupPage/GroupList.tsx
+++ src/components/GroupPage/GroupList.tsx
@@ -1,13 +1,24 @@
 
 import React from 'react'
 import { getUserGroups } from '../../common/util/API'
+import CreateGroupModal from '../CreateModal/CreateGroupModal'
 import ListComponent from '../ListView/ListComponent'
 
 
 const GroupList = () => {
   return (
-    <div>
-        <ListComponent apiFunction={getUserGroups} />
+    <div className='divide-y-2 divide-grey-800 divide-solid'>
+      <div>      
+      <div className= "mt-3 flex mb-3 justify-start">
+        <div className='w-[53%]'></div>
+        <h1 className="text-3xl font-semibold w-[50%]">Groups</h1>
+        <div className="flex justify-start mr-3">
+          <CreateGroupModal></CreateGroupModal>
+        </div>
+
+        </div>    
+        </div>
+      <ListComponent apiFunction={getUserGroups} />
     </div>
   )
 }
