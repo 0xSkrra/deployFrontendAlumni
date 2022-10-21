592139923d7e207ebac1f4f0983166f88979b534
Espen Sjo <esjo20@gmail.com>
2022-10-21 12:23:24 +0200

    fix: remove unecessary search bar

--- src/components/Layout/navbar.tsx
+++ src/components/Layout/navbar.tsx
@@ -18,15 +18,7 @@ return (
     <h2 className="relative flex-col text-3xl font-semibold text-gray-800   ">
       Alumni Solution
     </h2>
-    <div className="relative flex-col mt-6  ">
-        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-            </svg>
-        </span>
 
-        <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search" />
-    </div>
 
     <div className="flex flex-col justify-between flex-1 mt-6">
         <nav>
