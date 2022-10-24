e13ad19eb4f170ae52b0dfd600490d54876a321d
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-24 14:40:57 +0200

    style: add some colors to listrow

--- src/components/GroupPage/GroupList.tsx
+++ src/components/GroupPage/GroupList.tsx
@@ -7,7 +7,7 @@ import ListComponent from '../ListView/ListComponent'
 
 const GroupList = () => {
   return (
-    <div className='divide-y-2 divide-grey-800 divide-solid'>
+    <div className='divide-y-2 divide-grey-800 divide-solid backgroundcolor3'>
       <div>      
       <div className= "pt-3 pb-7 flex justify-start backgroundColor ">
         <div className='w-[46%]'></div>
