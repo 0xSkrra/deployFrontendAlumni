89df330e39b03c70e8f1cf4b0507379f4a1ad730
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-24 11:21:02 +0200

    fix: centered new event button

--- src/components/CreateModal/CreateEventModal.tsx
+++ src/components/CreateModal/CreateEventModal.tsx
@@ -126,7 +126,7 @@ export default function CreateEventModal({id, target}: CreateEventProps) {
   return (
     <>
       <button
-        className="px-4 flex py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
+        className="text-justify justify-center px-4 flex py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
         type="button"
         onClick={() => setShowModal(true)}
       >
