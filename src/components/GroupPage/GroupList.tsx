3fc7c9a74a34cdb5cc088ce22ee2aab7af1f8229
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-24 13:59:21 +0200

    style: group list

--- src/components/GroupPage/GroupList.tsx
+++ src/components/GroupPage/GroupList.tsx
@@ -9,7 +9,7 @@ const GroupList = () => {
   return (
     <div className='divide-y-2 divide-grey-800 divide-solid'>
       <div>      
-      <div className= "pt-3 pb-5 flex justify-start bg-purple-500">
+      <div className= "pt-3 pb-5 flex justify-start backgroundColor mt-1">
         <div className='w-[46%]'></div>
         <h1 className="text-3xl font-semibold w-[38%] pt-3">Groups</h1>
         <div className="flex justify-start mr-3 mt-3">
